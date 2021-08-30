1b2b1410fb1aa9b6b24a0f35d6821c6ba9f289e2
k.goto <basketball0316pf@gmail.com>
2021-08-30 10:26:58 +0900

    fix

--- scraping/src/download.py
+++ scraping/src/download.py
@@ -1,6 +1,6 @@
-import utils
+import module
 import config
 
 if __name__ == "__main__":
-    slacker = utils.Slacker()
+    slacker = module.Slacker()
     slacker.post_file(file_path=config.article_bin_path)
